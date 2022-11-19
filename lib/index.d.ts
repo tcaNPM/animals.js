8ee163f5851c5cb32741f215ac92e045a6ec5439
Not TCA <realtcatech@gmail.com>
2022-11-19 18:56:12 +0800

    fix cat image type being required

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,7 @@
 export type CatImageType = "image" | "gif";
 
 declare module "animals.js" {
-  export function cat(type: CatImageType): Promise<string>;
+  export function cat(type?: CatImageType): Promise<string>;
   export function dog(): Promise<string>;
   export function fox(): Promise<string>;
   export function duck(): Promise<string>;
